224d49cff7af102e7721d1b5fcd1d5004fd71410
Keit Oliveira <keit.oliveira@icloud.com>
2024-06-30 18:22:01 -0300

    add gtm integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
+
 // https://astro.build/config
 export default defineConfig({
-  
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    })
+  ],
 });
 
 // export default {
